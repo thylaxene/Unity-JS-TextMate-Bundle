3bd6148fb92e001e312a927cf8a30659b35f75eb
thylaxene <thylaxene@gmail.com>
2011-06-08 15:14:28 +1000

    Might help if I add the bundle!

---
+++ Unity JavaScript.tmbundle/Templates/JS Class File.tmTemplate/default.js
@@ -0,0 +1,13 @@
+/* JS Class file
+	great for creating game data and when combined with 
+	C#'s powerful Class serilaization and XML exporter/importer, 
+	creates a real useful tool.
+*/
+
+class ${TM_NEW_FILE_BASENAME} {
+	public var Title : String = "${TM_NEW_FILE_BASENAME}";
+
+	function ${TM_NEW_FILE_BASENAME} () {
+		// empty constructor
+	}
+}
